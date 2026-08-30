a7e46a7a5cb319fb17fb7f286aed7d34179dad6a
SNG Viraj Reddy <vir200319@gmail.com>
2026-08-30 15:06:15 +0530

    add: readme + comments
    Signed-off-by: SNG Viraj Reddy <vir200319@gmail.com>

--- init.el
+++ init.el
@@ -1,9 +1,14 @@
 (setq inhibit-startup-message t) ;; no splash screen
 (setq visible-bell t)            ;; flash when bell rings
 
+(load-theme 'leuven-dark t)      ;; load theme obvio
+
+;; setup package repositories
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
 
+;; terminal package
 (use-package vterm
   :ensure t)
+
